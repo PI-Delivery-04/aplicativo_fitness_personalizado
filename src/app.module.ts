5cfd13765bdb68d3533cd765ea93c8c450e80c8e
NicolasClementedt <nicolasfernando724@gmail.com>
2026-01-28 11:03:14 -0300

    importacoes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-
+import { ProdService } from './data/services/prod.service';
 import { TreinoModule } from './treino/treino.module';
-
 import { MetaModule } from './meta/meta.module';
-
 import { UsuarioModule } from './usuario/usuario.module';
 import { AppController } from './app.controller';
-import { ProdService } from './data/services/prod.service';
 
 @Module({
   imports: [
